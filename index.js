2a85bfe86a32fceb1e7780d7cedc0e4d828725b2
Jesse Brault <jbrault@mac.com>
2022-08-21 20:19:07 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,91 @@
+import fs from 'fs/promises'
+import path from 'path'
+import http from 'http'
+import child_process from 'child_process'
+import util from 'util'
+
+const exec = util.promisify(child_process.exec)
+
+const getLogger = logFile => msg => {
+    const date = new Date().toUTCString()
+    console.log(`${date} ${msg}`)
+    logFile.write(`${date} ${msg}\n`)
+}
+
+const getConfigs = async log => {
+    try {
+        const configFiles = await fs.readdir(
+            path.join(process.cwd(), 'configs')
+        )
+        const configs = []
+        for (const configFileName of configFiles) {
+            if (configFileName.endsWith('.json')) {
+                const configRaw = await fs.readFile(
+                    path.join(process.cwd(), 'configs', configFileName)
+                )
+                configs.push(JSON.parse(configRaw))
+            }
+        }
+        return configs
+    } catch (err) {
+        log(err)
+    }
+}
+
+const getRepositoryAndAction = req => {
+    const url = new URL(req.url, `https://${req.headers.host}`)
+    const segments = url.pathname.split('/')
+    // segements[0] is '', segments[1] is repository, segments[2] is action
+    const repository = segments[1]
+    const action = segments[2]
+    return {
+        repository,
+        action
+    }
+}
+
+const main = async () => {
+    const log = getLogger(await fs.open('./index.log', 'a'))
+
+    const configs = await getConfigs(log)
+
+    configs.forEach(config => {
+        log(`loaded config for repository: ${config.repository}`)
+    })
+
+    const server = http.createServer(async (req, res) => {
+        res.write('thank you')
+        res.end()
+
+        const { repository, action } = getRepositoryAndAction(req)
+
+        const config = configs.find(config => config.repository === repository)
+        if (config !== undefined) {
+            const onSpec = config.on.find(onSpec => onSpec.action === action)
+            if (onSpec !== undefined) {
+                if (onSpec.script.endsWith('.sh')) {
+                    try {
+                        await exec(
+                            `${path.join(
+                                process.cwd(),
+                                'configs',
+                                onSpec.script
+                            )} &> ${path.join(
+                                process.cwd(),
+                                config.repository
+                            )}.log`
+                        )
+                    } catch (err) {
+                        log(err)
+                    }
+                } else {
+                    log(`unsupported script type: ${onSpec.script}`)
+                }
+            }
+        }
+    })
+
+    server.listen(4000, () => log('listening on port 4000'))
+}
+
+main()
